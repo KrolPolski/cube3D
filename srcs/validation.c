75de50c4fbdd6ea488890b2ad34396f8dc6e3f12
Thomas Parratt <tparratt@c1r5p13.hive.fi>
2024-08-29 15:20:04 +0300

    wall check in progress

--- srcs/validation.c
+++ srcs/validation.c
@@ -6,7 +6,7 @@
 /*   By: tparratt <tparratt@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 11:13:27 by tparratt          #+#    #+#             */
-/*   Updated: 2024/08/29 13:23:13 by tparratt         ###   ########.fr       */
+/*   Updated: 2024/08/29 15:19:06 by tparratt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,124 +32,124 @@ static int  all_remaining(int i, char **arr)
     return (1);
 }
 
-static int  surrounded(char **map, size_t i, size_t j) //rename function
-{
-    int res;
-    int len;
+// static int  check_inlets(char **map, size_t i, size_t j)
+// {
+//     int res;
 
-    res = 0;
-    len = ft_strlen(map[i]);
-    ft_printf("j = %d\n", j);
-    if (i != 0 && i != len_2d(map) && j != 0 && j != ft_strlen(map[i]) - 1)
-    {
-        ft_printf("FIRST\n");
-        if (map[i - 1][j] != '1' && map[i - 1][j] != ' ')
-            res = 1;
-        if (map[i + 1][j] != '1' && map[i + 1][j] != ' ')
-            res = 2;
-        if (map[i][j - 1] != '1' && map[i][j - 1] != ' ')
-            res = 3;
-        if (map[i][j + 1] != '1' && map[i][j + 1] != ' ')
-            res = 4;
-    }
-    else if (i == 0 && j != 0 && j != ft_strlen(map[i]) - 1)
-    {
-        ft_printf("SECOND\n");
-        if (map[i + 1][j] != '1' && map[i + 1][j] != ' ')
-            res = 5;
-        if (map[i][j - 1] != '1' && map[i][j - 1] != ' ')
-            res = 6;
-        if (map[i][j + 1] != '1' && map[i][j + 1] != ' ')
-            res = 7;
-    }
-    else if (i == len_2d(map) && j != 0 && j != ft_strlen(map[i]) - 1)
-    {
-        ft_printf("THIRD\n");
-        if (map[i - 1][j] != '1' && map[i - 1][j] != ' ')
-            res = 8;
-        if (map[i][j - 1] != '1' && map[i][j - 1] != ' ')
-            res = 9;
-        if (map[i][j + 1] != '1' && map[i][j + 1] != ' ')
-            res = 10; 
-    }
-    else if (j == 0 && i != 0 && i != len_2d(map))
-    {
-        ft_printf("FORTH\n");
-        if (map[i - 1][j] != '1' && map[i - 1][j] != ' ')
-            res = 11;
-        if (map[i + 1][j] != '1' && map[i + 1][j] != ' ')
-            res = 12;
-        if (map[i][j + 1] != '1' && map[i][j + 1] != ' ')
-            res = 13;
-    }
-    else if (j != ft_strlen(map[i]) - 1 && i != 0 && i != len_2d(map))
-    {
-        ft_printf("FIFTH\n");
-        if (map[i - 1][j] != '1' && map[i - 1][j] != ' ')
-            res = 14;
-        if (map[i + 1][j] != '1' && map[i + 1][j] != ' ')
-            res = 15;
-        if (map[i][j - 1] != '1' && map[i][j - 1] != ' ')
-            res = 16;
-    }
-    if (res > 0)
-    {
-        ft_printf("res = %d\n", res);
-        print_error("Map not surrounded by walls");
-    }
-    ft_printf("res = %d\n", res);
-    return (res);
-}
+//     res = 0;
+//     if (i != 0 && i != len_2d(map) && j != 0 && j != ft_strlen(map[i]) - 1)
+//     {
+//         if (map[i - 1][j] != '1' && map[i - 1][j] != ' ') // what if the character is on the wall and a different char?
+//             res = 1;
+//         if (map[i + 1][j] != '1' && map[i + 1][j] != ' ')
+//             res = 2;
+//         if (map[i][j - 1] != '1' && map[i][j - 1] != ' ')
+//             res = 3;
+//         if (map[i][j + 1] != '1' && map[i][j + 1] != ' ')
+//             res = 4;
+//     }
+//     else if (i == 0 && j != 0 && j != ft_strlen(map[i]) - 1)
+//     {
+//         if (map[i + 1][j] != '1' && map[i + 1][j] != ' ')
+//             res = 5;
+//         if (map[i][j - 1] != '1' && map[i][j - 1] != ' ')
+//             res = 6;
+//         if (map[i][j + 1] != '1' && map[i][j + 1] != ' ')
+//             res = 7;
+//     }
+//     else if (i == len_2d(map) && j != 0 && j != ft_strlen(map[i]) - 1)
+//     {
+//         if (map[i - 1][j] != '1' && map[i - 1][j] != ' ')
+//             res = 8;
+//         if (map[i][j - 1] != '1' && map[i][j - 1] != ' ')
+//             res = 9;
+//         if (map[i][j + 1] != '1' && map[i][j + 1] != ' ')
+//             res = 10; 
+//     }
+//     else if (j == 0 && i != 0 && i != len_2d(map))
+//     {
+//         if (map[i - 1][j] != '1' && map[i - 1][j] != ' ')
+//             res = 11;
+//         if (map[i + 1][j] != '1' && map[i + 1][j] != ' ')
+//             res = 12;
+//         if (map[i][j + 1] != '1' && map[i][j + 1] != ' ')
+//             res = 13;
+//     }
+//     else if (j != ft_strlen(map[i]) - 1 && i != 0 && i != len_2d(map))
+//     {
+//         if (map[i - 1][j] != '1' && map[i - 1][j] != ' ')
+//             res = 14;
+//         if (map[i + 1][j] != '1' && map[i + 1][j] != ' ')
+//             res = 15;
+//         if (map[i][j - 1] != '1' && map[i][j - 1] != ' ')
+//             res = 16;
+//     }
+//     if (res > 0)
+//         print_error("Map not surrounded by walls");
+//     return (res);
+// }
 
-static void check_walls(t_map *map)
-{
-    size_t i;
-    size_t len;
-    size_t j;
+// static void check_walls(t_map *map)
+// {
+//     size_t i;
+//     size_t len;
+//     size_t j;
     
-    i = 0;
-    len = len_2d(map->map);
-    while (map->map[0][i] || map->map[len][i]) //first and last rows
-    {
-        if (map->map[0][i] != '1' && map->map[0][i] != ' ')
-            print_error("Map not surrounded by walls");
-        if (map->map[len][i] != '1' && map->map[len][i] != ' ')
-            print_error("Map not surrounded by walls");
-        i++;
-    }
-    i = 0;
-    while (map->map[i]) // first char in row
-    {
-        j = 0;
-        while (map->map[i][j] == ' ')
-        {
-            surrounded(map->map, i , j);
-            j++;
-        }
-        if (map->map[i][j] != '1')
-        {
-            //ft_putendl_fd(map->map[i], 1);
-            print_error("Map not surrounded by walls");
-        }
-        i++;
-    }
-    i = 0;
-    while (map->map[i]) // last char in row
-    {
-        j = ft_strlen(map->map[i]) - 1;
-        while (map->map[i][j] == ' ')
-        {
-            surrounded(map->map, i, j);
-            j--;
-        }
-        if (map->map[i][j] != '1')
-        {
-            //ft_putendl_fd(map->map[i], 1);
-            print_error("Map not surrounded by walls");
-        }
-        i++;
-    }
-}
+//     i = 0;
+//     len = len_2d(map->map);
+//     j = 0;
+//     while (map->map[i][j]) // first char in column
+//     {
+//         i = 0;
+//         while (map->map[i][j] == ' ')
+//         {
+//             check_inlets(map->map, i, j);
+//             i++;
+//         }
+//         if (map->map[i][j] != '1')
+//             print_error("Map not surrounded by walls");
+//         j++;
+//     }
+//     j = 0;
+//     while (map->map[i][j]) // last char in column
+//     {
+//         i = len_2d(map->map);
+//         while (map->map[i][j] == ' ')
+//         {
+//             check_inlets(map->map, i, j);
+//             i--;
+//         }
+//         if (map->map[i][j] != '1')
+//             print_error("Map not surrounded by walls");
+//         j++;
+//     }
+//     i = 0;
+//     while (map->map[i][j]) // first char in row
+//     {
+//         j = 0;
+//         while (map->map[i][j] == ' ')
+//         {
+//             check_inlets(map->map, i , j);
+//             j++;
+//         }
+//         if (map->map[i][j] != '1')
+//             print_error("Map not surrounded by walls");
+//         i++;
+//     }
+//     i = 0;
+//     while (map->map[i][j]) // last char in row
+//     {
+//         j = ft_strlen(map->map[i]) - 1;
+//         while (map->map[i][j] == ' ')
+//         {
+//             check_inlets(map->map, i, j);
+//             j--;
+//         }
+//         if (map->map[i][j] != '1')
+//             print_error("Map not surrounded by walls");
+//         i++;
+//     }
+// }
 
 static void valid_chars(char c)
 {
@@ -214,5 +214,5 @@ void    validate(t_map *map)
 {
     find_empty_line(map);
     check_chars(map);
-    check_walls(map);
+    //check_walls(map);
 }
