cf7a533292e3fd30af14e999831d5f85617e05b1
Ryan Boudwin <ryanboudwin@outlook.com>
2024-09-02 13:49:45 +0300

    Now initializing player position and orientation correctly

--- srcs/raycaster.c
+++ srcs/raycaster.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 08:56:04 by rboudwin          #+#    #+#             */
-/*   Updated: 2024/09/02 11:57:54 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/09/02 13:48:35 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,7 +117,7 @@ void	ft_single_press_hook(mlx_key_data_t keydata, void *param)
 		{
 			info->map_visible = false;
 			info->img->map->enabled = false;
-			//info->img->plyr->enabled = false;
+			info->img->plyr->enabled = false;
 			//info->img->fg->enabled = false;
 			ft_printf("2D map turned off\n");
 		}
@@ -125,7 +125,7 @@ void	ft_single_press_hook(mlx_key_data_t keydata, void *param)
 		{
 			info->map_visible = true;
 			info->img->map->enabled = true;
-			//info->img->plyr->enabled = true;
+			info->img->plyr->enabled = true;
 			//info->img->fg->enabled = true;
 			ft_printf("2D map turned on\n");
 		}
@@ -142,12 +142,61 @@ void init_img(t_info *info)
 	info->img->fg = mlx_new_image(info->mlx, info->s_width, info->s_height);
 	info->img->world = mlx_new_image(info->mlx, info->s_width, info->s_height);
 	info->img->map = mlx_new_image(info->mlx, info->map_width, info->map_height);
+	info->img->plyr = mlx_new_image(info->mlx, info->map_width, info->map_height);
 	ft_memset(info->img->bg->pixels, 180, info->img->bg->width
 		* info->img->bg->height * BPP);
 	mlx_image_to_window(info->mlx, info->img->bg, 0, 0);
 	mlx_image_to_window(info->mlx, info->img->world, 0, 0);
 	ft_memset(info->img->map->pixels, 255, info->img->map->width
 		* info->img->map->height * BPP);
+}
+
+void init_plyr(t_info *info, t_map *map)
+{
+	int y;
+	int x;
+	int found;
+	found = 0;
+	y = 0;
+	x = 0;
+	while (map->map[y])
+	{
+		while (map->map[y][x])
+		{
+			if (ft_strchr("NESW", map->map[y][x]))
+			{
+				found = 1;
+				break ;
+			}
+			x++;
+		}
+		if (!found)
+		{
+			x = 0;
+			y++;
+		}
+		else
+			break;
+	}
+	if (!found)
+		ft_putstr_fd("Error: No starting position found\n", 2);
+	ft_printf("We have found the starting position at y: %d x: %d\n", y, x);
+	info->p_x = x + 0.5;
+	info->p_y = y + 0.5;
+	info->p_orient = 42;
+	if (map->map[y][x] == 'N')
+		info->p_orient = M_PI + M_PI_2;
+	else if (map->map[y][x] == 'E')
+		info->p_orient = 0;
+	else if (map->map[y][x] == 'S')
+		info->p_orient = M_PI_2;
+	else if (map->map[y][x] == 'W')
+		info->p_orient = M_PI;
+	else
+	 	ft_putstr_fd("Could not set player orientation\n", 2);
+	printf("Our starting position is y: %f, x: %f, orientation is %f\n", info->p_y, info->p_x, info->p_orient); // this doesn't work because
+	//ft_printf doesn't handle floats
+	
 }
 
 void raycaster(mlx_t *mlx, t_map *map, t_images *img)
@@ -175,10 +224,10 @@ void draw_squares(mlx_t *mlx, t_map *map, t_info *info)
 		while(map->map[y][x] != '\0')
 		{
 			//ft_printf("inside second while loop\n");
-			ft_printf("%c", map->map[y][x]);
-			if (map->map[y][x] == '0')
+			//ft_printf("%c", map->map[y][x]);
+			if (map->map[y][x] == '0' || ft_strchr("NESW", map->map[y][x]))
 			{
-				ft_printf("found a 0\n");
+			//	ft_printf("found a 0\n");
 				while (k < map->sq)
 				{
 					while (i < map->sq)
@@ -195,7 +244,7 @@ void draw_squares(mlx_t *mlx, t_map *map, t_info *info)
 			}
 			else if (map->map[y][x] == '1')
 			{
-				ft_printf("found a 0\n");
+				//ft_printf("found a 0\n");
 				while (k < map->sq)
 				{
 					while (i < map->sq)
@@ -222,10 +271,17 @@ void draw_squares(mlx_t *mlx, t_map *map, t_info *info)
 	
 }
 
+void draw_2d_player(mlx_t *mlx, t_map *map, t_info *info)
+{
+	
+}
+
 void draw_2d_map(mlx_t *mlx, t_map *map, t_info *info)
 {	
 	ft_printf("Drawing 2d map\n");
 	mlx_image_to_window(info->mlx, info->img->map, info->map_width / 8, info->map_height / 8);
+	mlx_image_to_window(info->mlx, info->img->plyr, info->map_width / 8, info->map_height / 8);
+	
 	map->x_len = 0;
 	map->y_len = 0;
 	ft_printf("map->map[0] is '%s'\n", map->map[0]);
@@ -243,6 +299,7 @@ void draw_2d_map(mlx_t *mlx, t_map *map, t_info *info)
 		map->sq = map->sq_h;
 	printf("Sq_w: %d\n Sq_h: %d\n sq: %d\n", map->sq_w, map->sq_h, map->sq);
 	draw_squares(mlx, map, info);
+	draw_2d_player(mlx, map, info);
 }
 
 void setup_mlx(t_map *map)
@@ -264,6 +321,7 @@ void setup_mlx(t_map *map)
 		ft_putstr_fd("MLX initialization error\n", 2);
 	}
 	init_img(&info);
+	init_plyr(&info, info.map);
 	int i = 0;
 	ft_printf("Loading map: \n");
 	print_2d(map->map);
