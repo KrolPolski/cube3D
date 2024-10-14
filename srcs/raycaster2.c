21c12cd9cdb988bcdb6fb1353ae63822389f1b63
Ryan Boudwin <rboudwin@student.hive.fi>
2024-10-14 11:50:45 +0300

    no longer segfaulting on mlx_put_pixel

--- srcs/raycaster2.c
+++ srcs/raycaster2.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 10:46:43 by rboudwin          #+#    #+#             */
-/*   Updated: 2024/09/26 11:00:42 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/10/14 11:49:53 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -199,8 +199,14 @@ void cast_wall(double ray_len, int i, t_info *info, t_images *img, enum e_inters
     top_pixel = info->s_height / 2 - column_height / 2;
     if (top_pixel < 0)
         top_pixel = 0;
-    while (pixels < column_height)
+    printf("We are trying to draw a column at x: %d\n", i);
+    while (pixels < column_height - 1)
     {
+        if (top_pixel + pixels > info->s_height - 1 || top_pixel + pixels < 0 || i > info->s_width || i < 0)
+        {
+            printf("tried to draw out of bounds\n");
+            break;
+        }
         mlx_put_pixel(img->world, i, top_pixel + pixels, color);
         pixels++;
     }
