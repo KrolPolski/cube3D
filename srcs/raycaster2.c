82c6fe415a2f179c5687aea48735aff1eab5107a
Ryan Boudwin <rboudwin@student.hive.fi>
2024-10-21 15:56:17 +0300

    refactored cast_wall

--- srcs/raycaster2.c
+++ srcs/raycaster2.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 10:46:43 by rboudwin          #+#    #+#             */
-/*   Updated: 2024/10/21 15:44:02 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/10/21 15:55:29 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -199,26 +199,52 @@ unsigned int	find_x_percent(t_info *info, enum e_intersect inter)
 	return (int_percent);
 }
 
+void	draw_wall(t_cw *cw, t_info *info, unsigned int i)
+{
+	if (cw->column_height > info->s_height)
+		cw->column_height = info->s_height;
+	while (cw->pixels < cw->column_height - 1)
+	{
+		if (cw->top_pixel + cw->pixels > info->s_height - 1 || cw->top_pixel
+			+ cw->pixels < 0 || i > info->s_width || i < 0)
+		{
+			break ;
+		}
+		mlx_put_pixel(info->img->world, i, cw->top_pixel + cw->pixels,
+			cw->color);
+		cw->pixels++;
+		cw->texel_y_int = (int)cw->texel_y;
+		cw->color = mlx_get_pixel(cw->texture_img, cw->texel, cw->texel_y_int);
+		cw->texel_y += cw->texel_step;
+	}
+}
+
+void	init_cast_wall(t_cw *cw, t_info *info, double ray_len,
+	enum e_intersect inter)
+{
+	cw->y = 0;
+	cw->texture_img = get_wall_texture(info->img, info->ray_orient, inter);
+	cw->x_percent = find_x_percent(info, inter);
+	cw->top_pixel = 0;
+	cw->pixels = 0;
+	cw->angle_diff = cos(info->ray_orient - info->p_orient);
+	if (fabs(cw->angle_diff) > EPSILON)
+		cw->column_height = (info->s_height / (ray_len
+					* cw->angle_diff));
+	else
+		cw->column_height = info->s_height / ray_len;
+	cw->top_pixel = info->s_height / 2 - cw->column_height / 2;
+	cw->texel = cw->texture_img->width * cw->x_percent / 100;
+}
+
 void	cast_wall(double ray_len, unsigned int i, t_info *info,
 	enum e_intersect inter)
 {
-	t_cw cw;
+	t_cw	cw;
 
-	cw.y = 0;
-	cw.texture_img = get_wall_texture(info->img, info->ray_orient, inter);
-	cw.x_percent = find_x_percent(info, inter);
 	if (ray_len > info->rend_dist)
 		return ;
-	cw.top_pixel = 0;
-	cw.pixels = 0;
-	cw.angle_diff = cos(info->ray_orient - info->p_orient);
-	if (fabs(cw.angle_diff) > EPSILON)
-		cw.column_height = (info->s_height / (ray_len
-			* cw.angle_diff));
-	else
-		cw.column_height = info->s_height / ray_len;
-	cw.top_pixel = info->s_height / 2 - cw.column_height / 2;
-	cw.texel = cw.texture_img->width * cw.x_percent / 100;
+	init_cast_wall(&cw, info, ray_len, inter);
 	if (cw.texture_img->height != 0)
 		cw.pixels_per_texel = round((double)cw.column_height
 				/ (double)cw.texture_img->height);
@@ -226,7 +252,8 @@ void	cast_wall(double ray_len, unsigned int i, t_info *info,
 		return ;
 	cw.color = mlx_get_pixel(cw.texture_img, cw.texel, 0);
 	if (cw.column_height > EPSILON)
-		cw.texel_step = (double)cw.texture_img->height / (double)cw.column_height;
+		cw.texel_step = (double)cw.texture_img->height
+			/ (double)cw.column_height;
 	else
 		return ;
 	cw.texel_y = 0;
@@ -235,21 +262,7 @@ void	cast_wall(double ray_len, unsigned int i, t_info *info,
 		cw.texel_y = -(cw.top_pixel * cw.texel_step);
 		cw.top_pixel = 0;
 	}
-	if (cw.column_height > info->s_height)
-		cw.column_height = info->s_height;
-	while (cw.pixels < cw.column_height - 1)
-	{
-		if (cw.top_pixel + cw.pixels > info->s_height - 1 || cw.top_pixel + cw.pixels < 0
-			|| i > info->s_width || i < 0)
-		{
-			break ;
-		}
-		mlx_put_pixel(info->img->world, i, cw.top_pixel + cw.pixels, cw.color);
-		cw.pixels++;
-		cw.texel_y_int = (int)cw.texel_y;
-		cw.color = mlx_get_pixel(cw.texture_img, cw.texel, cw.texel_y_int);
-		cw.texel_y += cw.texel_step;
-	}
+	draw_wall(&cw, info, i);
 }
 
 void	raycaster(t_map *map, t_images *img, t_info *info)
@@ -263,10 +276,8 @@ void	raycaster(t_map *map, t_images *img, t_info *info)
 	info->ray_x = info->p_x;
 	info->ray_y = info->p_y;
 	info->ray_orient = info->p_orient - (M_PI / 6);
-	ft_memset(img->world->pixels, 0, img->world->height * img->world->width * BPP);
-	//mlx_delete_image(mlx, img->world);
-	//img->world = mlx_new_image(mlx, info->s_width, info->s_height);
-	//mlx_image_to_window(mlx, img->world, 0, 0);
+	ft_memset(img->world->pixels, 0, img->world->height * img->world->width
+		* BPP);
 	i = 0;
 	while (i < info->s_width)
 	{
