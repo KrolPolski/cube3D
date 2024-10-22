6982b2371f468d637347dbcc50925db19af5c3f6
Thomas Parratt <tparratt@c2r2p2.hive.fi>
2024-10-22 10:25:06 +0300

    no segfault with incorrect no of args

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: tparratt <tparratt@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 11:12:58 by tparratt          #+#    #+#             */
-/*   Updated: 2024/10/21 17:24:04 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/10/22 10:23:16 by tparratt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int	main(int argc, char **argv)
 	t_map	map;
 
 	if (argc != 2)
-		ft_putendl_fd("Error\nIncorrect number of arguments", 2);
-	else
 	{
-		parse(argv, &map);
-		validate(&map);
+		ft_putendl_fd("Error\nIncorrect number of arguments", 2);
+		return (1);
 	}
+	parse(argv, &map);
+	validate(&map);
 	setup_mlx(&map);
 	free_map(&map);
 	return (0);
